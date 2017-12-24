9ff10e90b562bc794de35a51df4f4304e392ee0a
Nitish Sharma <nitish.sharma@keepworks.com>
2017-12-24 21:27:30 +0530

    Modify seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,66 @@
 AdminUser.create!(email: 'admin@catalogure.com', password: 'qweqweqwe', password_confirmation: 'qweqweqwe')
 
-1.upto(9) do |i|
-  category = Category.create!(name: "category #{i}")
+category = Category.create!(name: 'Books')
 
-  1.upto(2) do |j|
-    sub_category = category.children.create!(name: "sub_category #{i}.#{j}")
+sub_category = category.children.create!(name: 'Genre')
 
-    1.upto(3) do |k|
-      sub_sub_category = sub_category.children.create!(name: "sub_sub_category #{i}.#{j}.#{k}")
-      Product.create!(categories: [sub_sub_category], name: "product #{i}.#{j}.#{k}", description: FFaker::Lorem.paragraph(sentence_count = 8), price_in_sgd: rand(1..100)) if k.even?
-    end
-  end
-end
+sub_sub_category = sub_category.children.create!(name: 'Fantasy')
+sub_sub_category = sub_category.children.create!(name: 'Horror')
+sub_sub_category = sub_category.children.create!(name: 'Mystery')
+sub_sub_category = sub_category.children.create!(name: 'Romance')
+
+sub_category = category.children.create!(name: 'Author')
+
+sub_sub_category = sub_category.children.create!(name: 'J. K. Rowling')
+sub_sub_category = sub_category.children.create!(name: 'Stephen King')
+sub_sub_category = sub_category.children.create!(name: 'Dan Brown')
+sub_sub_category = sub_category.children.create!(name: 'Erich Segal')
+
+category = Category.create!(name: 'Clothing')
+
+sub_category = category.children.create!(name: 'Material')
+
+sub_sub_category = sub_category.children.create!(name: 'Cotton')
+sub_sub_category = sub_category.children.create!(name: 'Polyester')
+sub_sub_category = sub_category.children.create!(name: 'Denim')
+sub_sub_category = sub_category.children.create!(name: 'Leather')
+
+sub_category = category.children.create!(name: 'Type')
+
+sub_sub_category = sub_category.children.create!(name: 'T Shirt')
+sub_sub_category = sub_category.children.create!(name: 'Trouser')
+sub_sub_category = sub_category.children.create!(name: 'Shorts')
+sub_sub_category = sub_category.children.create!(name: 'Jackets')
+
+category = Category.create!(name: 'Movies')
+
+sub_category = category.children.create!(name: 'Genre')
+
+sub_sub_category = sub_category.children.create!(name: 'Action')
+sub_sub_category = sub_category.children.create!(name: 'Science Fiction')
+sub_sub_category = sub_category.children.create!(name: 'Drama')
+sub_sub_category = sub_category.children.create!(name: 'Documentary')
+
+sub_category = category.children.create!(name: 'Language')
+
+sub_sub_category = sub_category.children.create!(name: 'English')
+sub_sub_category = sub_category.children.create!(name: 'French')
+sub_sub_category = sub_category.children.create!(name: 'German')
+sub_sub_category = sub_category.children.create!(name: 'Spanish')
+
+category = Category.create!(name: 'Laptops')
+
+sub_category = category.children.create!(name: 'Brand')
+
+sub_sub_category = sub_category.children.create!(name: 'Apple')
+sub_sub_category = sub_category.children.create!(name: 'Dell')
+sub_sub_category = sub_category.children.create!(name: 'Lenovo')
+sub_sub_category = sub_category.children.create!(name: 'HP')
+
+sub_category = category.children.create!(name: 'Screen')
+
+sub_sub_category = sub_category.children.create!(name: '11 inch')
+sub_sub_category = sub_category.children.create!(name: '12 inch')
+sub_sub_category = sub_category.children.create!(name: '13 inch')
+sub_sub_category = sub_category.children.create!(name: '15 inch')
 
